e272282edf2e963150fcb5dfc9930ca5d4ad7ec2
Sharv Ambre <90981883+SharvAmbre@users.noreply.github.com>
2021-09-25 12:02:15 -0700

    Add files via upload

---
+++ C11/Pro-c11-sa1-template-main/sketch.js
@@ -0,0 +1,19 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var UwU = []
+function setup() {
+  createCanvas(400,400);
+s1 = createSprite(200,200,10,10)
+s2 = createSprite(400,400,20,20)
+UwU = [s1,s2]
+for (var i=0; i<UwU.length; i++) {
+console.log(UwU[i].position.x)
+}
+ }
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
